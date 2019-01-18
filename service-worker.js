a9c969258a2444891962d4494204c941dcfc1c53
Justin Lettau <justin.b.lettau@gmail.com>
2019-01-17 22:15:07 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-undraw/precache-manifest.af6932482ad623e009bc467bb0eb27a7.js"
+  "/react-undraw/precache-manifest.38fb64206ce5ae8930446cbc4599b3c6.js"
 );
 
 workbox.clientsClaim();
